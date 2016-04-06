4c3bdbd9bbc97c956bad11d22d932aa106a29073
Wiiitek <kubiczak.jakub@gmail.com>
2016-04-06 21:04:31 +0200

    http resources sample with snapshot dependencies

---
+++ http/resources/README.md
@@ -0,0 +1,15 @@
+
+pl.kubiczak.felix.shark.samples.http.resources
+==============================================
+
+Registration of bundle resources with HTTP Service.
+
+Code samples use `org.apache.felix.scr.annotations` and `maven-scr-plugin`
+for managing HTTP Service.
+
+Project site available at [http://site.kubiczak.pl/][project-site]
+
+Maven artifacts at [http://maven.kubiczak.pl/][custom-maven-repo]
+
+[project-site]: http://site.kubiczak.pl/
+[custom-maven-repo]: http://maven.kubiczak.pl/
---
+++ http/resources/src/main/java/pl/kubiczak/shark/samples/http/resources/ResourcesRegistration.java
@@ -0,0 +1,34 @@
+package pl.kubiczak.shark.samples.http.resources;
+
+import org.apache.felix.scr.annotations.*;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true)
+@Service(value = ResourcesRegistration.class)
+public class ResourcesRegistration {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@Reference(policy = ReferencePolicy.DYNAMIC)
+	private volatile HttpService httpService;
+
+	@Activate
+	public void start() {
+		try {
+			httpService.registerResources("/shark/samples/http/resources", "/", null);
+			httpService.registerResources("/shark/samples/http/resources/img", "/img", null);
+			log.debug("Resources registered");
+		} catch (NamespaceException ne) {
+			log.warn("Failed to register resources", ne);
+		}
+	}
+
+	@Deactivate
+	public void stop() {
+		httpService.unregister("/shark/samples/http/resources");
+		httpService.unregister("/shark/samples/http/resources/img");
+	}
+}
