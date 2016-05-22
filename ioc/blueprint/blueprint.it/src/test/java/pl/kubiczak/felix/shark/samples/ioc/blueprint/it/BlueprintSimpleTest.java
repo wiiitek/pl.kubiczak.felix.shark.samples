79e1559416cdd85f6e721e85af98d859194d6b05
Wiiitek <kubiczak.jakub@gmail.com>
2016-05-22 20:29:27 +0200

    sample module for pax exam tests

---
+++ ioc/blueprint/blueprint.it/src/test/java/pl/kubiczak/felix/shark/samples/ioc/blueprint/it/BlueprintSimpleTest.java
@@ -0,0 +1,90 @@
+package pl.kubiczak.felix.shark.samples.ioc.blueprint.it;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.util.Filter;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+
+import pl.kubiczak.felix.shark.samples.ioc.blueprint.simple.EventHandlerImpl;
+
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class BlueprintSimpleTest {
+
+	private static final Map<String, String> TEST_EVENT_PROPERTIES = new HashMap<String, String>() {{
+		put("test.event.title", "Test Event Title");
+		put("test.event.description", "Test Event Description");
+	}};
+
+	@Inject
+	private EventAdmin eventAdmin;
+
+	/**
+	 * EventHandler implementation from blueprint.simple bundle
+	 */
+	@Inject
+	@Filter("(" + EventConstants.EVENT_TOPIC + "=" + EventHandlerImpl.TOPIC + ")")
+	private EventHandler eventHandler;
+
+	@Configuration
+	public Option[] provideRequiredBundles() {
+		return new Option[]{
+				junitBundles(),
+				// aries blueprint
+				mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint.api").versionAsInProject(),
+				mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint.core").versionAsInProject(),
+				mavenBundle("org.apache.aries", "org.apache.aries.util").versionAsInProject(),
+				mavenBundle("org.apache.aries.proxy", "org.apache.aries.proxy").versionAsInProject(),
+				// bundles for tests
+				mavenBundle("org.apache.felix", "org.apache.felix.eventadmin").versionAsInProject(),
+				mavenBundle("pl.kubiczak.felix.shark.samples.ioc", "blueprint.simple").versionAsInProject()
+		};
+	}
+
+	@Test
+	public void eventAdminServiceShouldBeAvailable() {
+		assertThat(eventAdmin, is(not(nullValue())));
+	}
+
+	@Test
+	public void eventHandlerServiceShouldBeAvailable() {
+		assertThat(eventHandler, is(not(nullValue())));
+	}
+
+	@Test
+	public void filterShouldWorkCorrectly() {
+		assertThat(eventHandler, instanceOf(EventHandlerImpl.class));
+	}
+
+	@Test
+	public void shouldPostOsgiEvent() {
+		long before = ((EventHandlerImpl) eventHandler).processedEvents();
+		// sends event synchronously
+		eventAdmin.sendEvent(new Event(EventHandlerImpl.TOPIC, TEST_EVENT_PROPERTIES));
+		long after = ((EventHandlerImpl) eventHandler).processedEvents();
+		assertThat(after, is(before + 1));
+	}
+
+}
---
+++ ioc/blueprint/blueprint.simple/src/main/java/pl/kubiczak/felix/shark/samples/ioc/blueprint/simple/EventHandlerImpl.java
@@ -0,0 +1,35 @@
+package pl.kubiczak.felix.shark.samples.ioc.blueprint.simple;
+
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventHandlerImpl implements EventHandler {
+
+	public static final String TOPIC = "pl/kubiczak/felix/shark/samples/ioc/blueprint/simple/EventHandlerImpl/Topic";
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	private final AtomicLong processedEvents = new AtomicLong(0);
+
+	private final DateFormatter dateFormatter;
+
+	public EventHandlerImpl(DateFormatter dateFormatter) {
+		this.dateFormatter = dateFormatter;
+	}
+
+	public void handleEvent(Event event) {
+		long counter = processedEvents.incrementAndGet();
+		log.debug("[{}]: current time: '{}', handling event: '{}'",
+				counter, dateFormatter.getFormatted(new Date()), event.getTopic());
+	}
+
+	public long processedEvents() {
+		return processedEvents.get();
+	}
+}
