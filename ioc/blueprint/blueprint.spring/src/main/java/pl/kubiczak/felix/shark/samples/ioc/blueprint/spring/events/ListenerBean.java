fdbe8c01475cbd38497105b50713bbd2de0e0133
Wiiitek <kubiczak.jakub@gmail.com>
2016-05-23 19:54:42 +0200

    spring testing example: ApplicationEventPublisher

---
+++ ioc/blueprint/blueprint.spring/src/main/java/pl/kubiczak/felix/shark/samples/ioc/blueprint/spring/events/ListenerBean.java
@@ -0,0 +1,24 @@
+package pl.kubiczak.felix.shark.samples.ioc.blueprint.spring.events;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class ListenerBean implements ApplicationListener {
+
+	private static final AtomicLong processedEvents = new AtomicLong();
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	public void onApplicationEvent(ApplicationEvent event) {
+		log.debug("processing application event: '{}'", event.getSource());
+		processedEvents.getAndIncrement();
+	}
+
+	public long processedEvents() {
+		return processedEvents.get();
+	}
+}
