3bec5450146a0ca06f55a02d6a78a62b016c6ae3
Wiiitek <kubiczak.jakub@gmail.com>
2016-06-14 00:53:30 +0200

    tabs to spaces in groovy files

--- samples.webconsole.security/src/test/groovy/pl/kubiczak/felix/shark/samples/security/BasicHttpAuthDecoderTest.groovy
+++ samples.webconsole.security/src/test/groovy/pl/kubiczak/felix/shark/samples/security/BasicHttpAuthDecoderTest.groovy
@@ -15,43 +15,43 @@ import static org.mockito.Mockito.when
  */
 class BasicHttpAuthDecoderTest extends Specification {
 
-	@Unroll
-	def "should decode '#username' and '#password' from #headerValue"() {
+    @Unroll
+    def "should decode '#username' and '#password' from #headerValue"() {
 
-		given:
-		HttpServletRequest mockHttpServletRequest = mock(HttpServletRequest.class)
-		when(mockHttpServletRequest.getHeader(eq("Authorization"))).thenReturn(headerValue);
+        given:
+        HttpServletRequest mockHttpServletRequest = mock(HttpServletRequest.class)
+        when(mockHttpServletRequest.getHeader(eq("Authorization"))).thenReturn(headerValue);
 
-		BasicHttpAuthDecoder tested = new BasicHttpAuthDecoder(mockHttpServletRequest)
+        BasicHttpAuthDecoder tested = new BasicHttpAuthDecoder(mockHttpServletRequest)
 
-		expect:
-		tested.retrieveDecodedCredentials() == new ImmutablePair<String, String>(username, password)
+        expect:
+        tested.retrieveDecodedCredentials() == new ImmutablePair<String, String>(username, password)
 
-		where:
-		headerValue                                                          | username            | password
-		"Basic dXNlcm5hbWU6cGFzc3dvcmQ="                                     | "username"          | "password"
-		"Basic dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
-		"Basic dXNlcm5hbWU6Q3VTVz91RHBPR2hkXl5HYTdqSFE="                     | "username"          | "CuSW?uDpOGhd^^Ga7jHQ"
-		"Basic dXNlcm5hbWU6"                                                 | "username"          | ""
-		"Basic OnBhc3N3b3Jk"                                                 | ""                  | "password"
-		"Basic Og=="                                                         | ""                  | ""
-		"Basic YTrFvA=="                                                     | "a"                 | "ż"
-		"Basic emHFvMOzxYLEhzptw7xkZSBiw7h0dGUgwr8="                         | "zażółć"            | "müde bøtte ¿"
-		"Basic dGVzdDo6Ojo="                                                 | "test"              | ":::"
-		"Basic fiFAIyQlXiYqKClfK3t9fDoiPD4/YDEyMzQ1OjY3ODkwLT1bXVw7JywuLw==" | "~!@#\$%^&*()_+{}|" | "\"<>?`12345:67890-=[]\\;',./"
+        where:
+        headerValue                                                          | username            | password
+        "Basic dXNlcm5hbWU6cGFzc3dvcmQ="                                     | "username"          | "password"
+        "Basic dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
+        "Basic dXNlcm5hbWU6Q3VTVz91RHBPR2hkXl5HYTdqSFE="                     | "username"          | "CuSW?uDpOGhd^^Ga7jHQ"
+        "Basic dXNlcm5hbWU6"                                                 | "username"          | ""
+        "Basic OnBhc3N3b3Jk"                                                 | ""                  | "password"
+        "Basic Og=="                                                         | ""                  | ""
+        "Basic YTrFvA=="                                                     | "a"                 | "ż"
+        "Basic emHFvMOzxYLEhzptw7xkZSBiw7h0dGUgwr8="                         | "zażółć"            | "müde bøtte ¿"
+        "Basic dGVzdDo6Ojo="                                                 | "test"              | ":::"
+        "Basic fiFAIyQlXiYqKClfK3t9fDoiPD4/YDEyMzQ1OjY3ODkwLT1bXVw7JywuLw==" | "~!@#\$%^&*()_+{}|" | "\"<>?`12345:67890-=[]\\;',./"
 
-		"BASIC dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
-		"basic dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
-		"BAsiC dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
+        "BASIC dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
+        "basic dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
+        "BAsiC dXNlcm5hbWU6UEBzc3cwcmQ="                                     | "username"          | "P@ssw0rd"
 
-		null                                                                 | null                | null
-		""                                                                   | null                | null
-		"A"                                                                  | null                | null
-		"Basic"                                                              | null                | null
-		"Basic "                                                             | null                | null
-		"Basic 123"                                                          | null                | null
-		"Basic MDAwMDAwMDA="                                                 | null                | null
-		"Adv   dXNlcm5hbWU6UEBzc3cwcmQ="                                     | null                | null
-		"      dXNlcm5hbWU6UEBzc3cwcmQ="                                     | null                | null
-	}
+        null                                                                 | null                | null
+        ""                                                                   | null                | null
+        "A"                                                                  | null                | null
+        "Basic"                                                              | null                | null
+        "Basic "                                                             | null                | null
+        "Basic 123"                                                          | null                | null
+        "Basic MDAwMDAwMDA="                                                 | null                | null
+        "Adv   dXNlcm5hbWU6UEBzc3cwcmQ="                                     | null                | null
+        "      dXNlcm5hbWU6UEBzc3cwcmQ="                                     | null                | null
+    }
 }
