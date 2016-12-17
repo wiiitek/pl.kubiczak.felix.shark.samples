7b094ae7d15082df2b8bfa273eb20028e4f7e112
Wiiitek <kubiczak.jakub@gmail.com>
2016-12-17 23:21:36 +0000

    fixes for spring security samples

--- samples.ioc/samples.ioc.blueprint.spring.security/src/main/java/pl/kubiczak/felix/shark/samples/ioc/blueprint/spring/security/ServletImpl.java
+++ samples.ioc/samples.ioc.blueprint.spring.security/src/main/java/pl/kubiczak/felix/shark/samples/ioc/blueprint/spring/security/ServletImpl.java
@@ -19,6 +19,8 @@ import javax.servlet.http.HttpServletResponse;
         (
                 service = Servlet.class,
                 property = {
+                        HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT + "="
+                                + ServletImpl.CONTEXT_FILTER,
                         HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN + "="
                                 + ServletImpl.SERVLET_PATTERN
                 }
@@ -26,11 +28,18 @@ import javax.servlet.http.HttpServletResponse;
         )
 public class ServletImpl extends HttpServlet {
 
+  /**
+   * Http context for Spring Security sample.
+   * The same context as Amdatu uses is used here.
+   */
+  public static final String CONTEXT_NAME = "org.osgi.service.http";
+
   static final String DATE_PATTERN = "%1$tY-%1$tm-%1$td %1$tH.%1$tM.%1$tS.%1$tL";
 
-  static final String PATH_PREFIX = "/samples.ioc.blueprint.spring.security";
+  static final String SERVLET_PATTERN = "/spring.security/*";
 
-  static final String SERVLET_PATTERN = PATH_PREFIX + "/*";
+  static final String CONTEXT_FILTER =
+          "(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME + "=" + CONTEXT_NAME + ")";
 
   private static final String RESPONSE_PATTERN =
           "Date = '%1s'\nRequestURI = '%2s'\nPathInfo = '%3s'\nQueryString = '%4s'";
