bb447a3112e9eb16a1978898c25d8f38a9da79a9
Wiiitek <kubiczak.jakub@gmail.com>
2017-05-27 00:45:37 +0200

    jersey test added

---
+++ samples.tests/samples.tests.functional/src/test/java/pl/kubiczak/felix/shark/samples/tests/functional/samples/jersey/SampleResourceTest.java
@@ -0,0 +1,25 @@
+package pl.kubiczak.felix.shark.samples.tests.functional.samples.jersey;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import pl.kubiczak.felix.shark.samples.tests.functional.SimpleHttpRequest;
+
+import java.io.IOException;
+
+public class SampleResourceTest {
+
+  private static final String TESTED_PAGE = "http://localhost:8080/jersey/sample";
+
+  @Test
+  public void jsonTest() throws IOException {
+    SimpleHttpRequest request = new SimpleHttpRequest(TESTED_PAGE);
+    JSONObject response = request.retrieveJson();
+
+    assertThat(response.getString("name"), equalTo("James Bond"));
+    assertThat(response.getInt("number"), equalTo(7));
+  }
+}
