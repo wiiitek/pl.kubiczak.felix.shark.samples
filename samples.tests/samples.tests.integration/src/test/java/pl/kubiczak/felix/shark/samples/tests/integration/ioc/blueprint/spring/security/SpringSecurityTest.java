346d1a0259b47f880f58ff241195fd923ad3c4f8
Wiiitek <kubiczak.jakub@gmail.com>
2017-05-27 12:34:31 +0200

    other Spring Security tests added

--- samples.tests/samples.tests.integration/src/test/java/pl/kubiczak/felix/shark/samples/tests/integration/ioc/blueprint/spring/security/SpringSecurityTest.java
+++ samples.tests/samples.tests.integration/src/test/java/pl/kubiczak/felix/shark/samples/tests/integration/ioc/blueprint/spring/security/SpringSecurityTest.java
@@ -1,6 +1,8 @@
 package pl.kubiczak.felix.shark.samples.tests.integration.ioc.blueprint.spring.security;
 
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
@@ -14,9 +16,10 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.ops4j.pax.exam.util.Filter;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 
+import pl.kubiczak.felix.shark.samples.ioc.blueprint.spring.security.UsernamePasswordAuthenticator;
 import pl.kubiczak.felix.shark.samples.tests.integration.ioc.LoggingOptions;
 import pl.kubiczak.felix.shark.samples.tests.integration.ioc.blueprint.Options;
 
@@ -24,18 +27,36 @@ import javax.inject.Inject;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class FilterChainTest {
-
-  private final Logger log = LoggerFactory.getLogger(getClass());
+public class SpringSecurityTest {
 
   @Inject
   @Filter("(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN
           + "=/samples.ioc.blueprint.spring.security/*)")
   private javax.servlet.Filter springSecurityFilterChain;
 
+  @Inject
+  private UsernamePasswordAuthenticator authenticator;
+
   @Test
   public void springSecurityFilterChainShouldBeAvailable() {
     assertThat(springSecurityFilterChain, instanceOf(javax.servlet.Filter.class));
+  }
+
+  @Test
+  public void authenticatorShouldBeAvailable() {
+    assertNotNull(authenticator);
+  }
+
+  @Test
+  public void authenticatorHasSpringAuthenticationManager() {
+    AuthenticationManager authenticationManager = authenticator.getAuthenticationManager();
+    assertNotNull(authenticationManager);
+  }
+
+  @Test
+  public void authenticatorDoesntAllowWithNullPassword() {
+    Authentication authentication = authenticator.authenticate("anonymous", null);
+    assertNull(authentication);
   }
 
   /**
